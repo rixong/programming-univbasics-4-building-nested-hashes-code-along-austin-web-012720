0cdac2b6be3e19c1d68f02844fb137156caa5a80
Rick Glascock <rixong@gmail.com>
2020-01-09 20:57:46 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,10 +1,16 @@
 def second_challenge
   epic_tragedy = {
     :montague => {
-      :patriarch => 
+      :patriarch => {},
+      :matriarch => {},
+      :hero => {}.
+      :hero_friends => []
       }, 
     :capulet => {
-      #your key/value pairs here!
+      :patriarch => {},
+      :matriarch => {},
+      :hero => {}.
+      :heroine_friends => []
       }}
 
   
